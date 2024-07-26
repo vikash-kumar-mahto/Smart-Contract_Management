4cdf3847049d5403b40efcefbed3173b7486632e
vikash kumar mahto <93486699+vikash-kumar-mahto@users.noreply.github.com>
2024-07-26 22:41:52 +0530

    chnage the code

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -2,36 +2,36 @@
 pragma solidity ^0.8.0;
 
 contract Bank {
-    mapping(address => string) accountNames;
+    mapping(address => string) userAccountNames;
 
     event Transfer(uint256 amount);
     event NameUpdate(string value);
 
     function setAccountName(string memory _name) public {
         require(
-            keccak256(abi.encodePacked(accountNames[msg.sender])) !=
+            keccak256(abi.encodePacked(userAccountNames[msg.sender])) !=
                 keccak256(abi.encodePacked(_name)),
             "New name is same as old name"
         );
-        accountNames[msg.sender] = _name;
+        userAccountNames[msg.sender] = _name;
 
-        emit NameUpdate(accountNames[msg.sender]);
+        emit NameUpdate(userAccountNames[msg.sender]);
     }
 
     function getAccountName() public view returns (string memory) {
-        return accountNames[msg.sender];
+        return userAccountNames[msg.sender];
     }
 
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function transferFunds(address payable _to) public payable {
-        _to.transfer(msg.value);
         if (msg.sender.balance < msg.value)
             revert
                 InsufficientBalance({
                     balance: msg.sender.balance,
                     withdrawAmount: msg.value
                 });
+        _to.transfer(msg.value);
         emit Transfer(msg.value);
     }
 
